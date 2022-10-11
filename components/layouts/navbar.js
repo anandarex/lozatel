91d4a3c71e99dbcc7c13c70702263fe218c8e57d
anandarex <rizkianandar@gmail.com>
2022-10-11 20:00:40 +0700

    changes

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -4,8 +4,8 @@ import ThemeToggleButton from './theme-toggle-button';
 
 const LinkItem = ({ href, path, children}) => {
     const active = path === href
-    const inactiveColor = useColorModeValue(`gray200`)
-    // const inactiveColor = useColorModeValue(`gray200`, `whiteAlpha.900`)
+    // const inactiveColor = useColorModeValue(`gray200`)
+    const inactiveColor = useColorModeValue(`gray200`, `whiteAlpha.900`)
     return(
         <NextLink href={href}>
             <Link
